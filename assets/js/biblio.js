62cd290ea6b6d1b34e942aaaf41cfb755f8c57b1
jairomelo <jairom@colmich.edu.mx>
2022-01-25 21:10:48 -0600

    bibliografía - base y antibiot

---
+++ assets/js/biblio.js
@@ -0,0 +1,7 @@
+function targetExternal() {
+    for (var links = document.links, i = 0, a; a = links[i]; i++) {
+        if (a.host !== location.host) {
+            a.target = '_blank';
+        }
+    }
+}
