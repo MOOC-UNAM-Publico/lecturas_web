037b9b445419877037bd76accbbc26542139c979
jairomelo <jairom@colmich.edu.mx>
2022-01-26 19:09:54 -0600

    template - urls

--- assets/js/template.js
+++ assets/js/template.js
@@ -4,9 +4,9 @@ class CustomFooterBib extends HTMLElement {
         <footer class="d-flex flex-wrap justify-content-end py-3 my-4 border-top">
             <div class="logos">
             
-            <img src="../../assets/imgs/UNAM_escudo_oficial.jpg" height="100" width="auto">
-            <img src="https://mooc.cuaieed.unam.mx/assets/images/cuaieed.png" height="auto" width="150">
-            <img src="https://mooc.cuaieed.unam.mx/assets/images/mooc.png" height="auto" width="150">
+            <a href="https://www.unam.mx/" target="_blank"><img src="../../assets/imgs/UNAM_escudo_oficial.jpg" height="100" width="auto"></a>
+            <a href="https://cuaieed.unam.mx/" target="_blank"><img src="https://mooc.cuaieed.unam.mx/assets/images/cuaieed.png" height="auto" width="150"></a>
+            <a href="https://mooc.cuaieed.unam.mx/" target="_blank"><img src="https://mooc.cuaieed.unam.mx/assets/images/mooc.png" height="auto" width="150"></a>
 
             </div>
       </footer>
